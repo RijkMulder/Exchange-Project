a03d96fb653e3ca902b166690b54161072983a43
villewilly <elias.nieminen@edu.hel.fi>
2024-05-14 11:03:38 +0300

    upgrade shop update

--- Assets/Scripts/UpgradeShopScript.cs
+++ Assets/Scripts/UpgradeShopScript.cs
@@ -3,25 +3,48 @@ using System.Collections.Generic;
 using UnityEngine;
 using TMPro;
 
+[System.Serializable]
+public class Prices
+{
+    public string type;
+    public int price;
+}
+
 public class UpgradeShopScript : MonoBehaviour
 {
     [SerializeField] int coins;
     [SerializeField] TextMeshProUGUI coinCountText;
     [SerializeField] TextMeshProUGUI assistText;
+    [SerializeField] Prices[] prices;
+    [SerializeField] TextMeshProUGUI[] buttonTexts;
+    
 
     void Update()
     {
         coinCountText.text = "COINS: " + coins.ToString();
     }
 
-    public void Buy(int cost)
+    public void Buy(string type)
     {
+        int cost = 0;
+        int currentArrayItem = 0;
+        for (int i = 0; i < prices.Length; i++)
+        {
+            if (prices[i].type == type)
+            {
+                cost = prices[i].price;
+                currentArrayItem = i;
+            }
+        }
         if (cost > 0)
         {
             if (coins > 0 && coins >= cost)
             {
                 coins -= cost;
                 assistText.text = "";
+                prices[currentArrayItem].price += 50;
+                buttonTexts[currentArrayItem].text = "+ " + prices[currentArrayItem].type.ToString() + " (" + prices[currentArrayItem].price.ToString() + " Coins)";
+                Debug.Log("bought: " + prices[currentArrayItem].type.ToString() + " for: " + cost);
             }
             else assistText.text = "Not enough money!";
         }
