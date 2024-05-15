1ad44ffdf2ded22ee1904b2961f6fd760d593749
villewilly <elias.nieminen@edu.hel.fi>
2024-05-15 13:58:09 +0300

    collectionUpdate

--- Assets/Scripts/LogBookScript.cs
+++ Assets/Scripts/LogBookScript.cs
@@ -1,12 +1,41 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using TMPro;
+using UnityEngine.UI;
+
+[System.Serializable]
+public class collection
+{
+    public string name;
+    public GameObject fishOBJ;
+    public TextMeshProUGUI sizeText;
+    public float highSize;
+    public bool unlocked;
+}
 
 public class LogBookScript : MonoBehaviour
 {
-    [SerializeField] List<string> logBookList = new List<string>();
-    public void checkLog(string type, int size)
+    [SerializeField] collection[] collection;
+
+    public void checkLog(string name, float size)
     {
-        
+        for (int i = 0; i < collection.Length; i++)
+        {
+            collection fish = collection[i];
+            if (fish.name == name)
+            {
+                if (!fish.unlocked)
+                {
+                    fish.unlocked = true;
+                    fish.fishOBJ.GetComponent<Image>().color = new Color32(255, 255, 255, 255);
+                    if (size > fish.highSize)
+                    {
+                        fish.highSize = size;
+                        fish.sizeText.text = "Size: " + fish.highSize.ToString();
+                    }
+                }
+            }
+        }
     }
 }
